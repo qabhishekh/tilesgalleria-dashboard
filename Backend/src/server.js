83c264a219ed01ae93676ae1a029cced0637608d
abhishekh-westack <abhishekh.ojha@westack.ai>
2025-10-07 00:22:58 +0530

    fix: change dbtest endpoint to use GET method for database connection check

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -82,7 +82,7 @@ app.use("/api/manualinvoices", manualInvoiceRoutes);
 app.use("/api/shipping", shippingRoutes);
 app.use("/api/password", passwordRoutes);
 app.use("/api/users", userRoutes);
-app.use("/", async (req, res) => {
+app.get("/api/dbtest", async (req, res) => {
   try {
     await connectDB(process.env.MONGODB_URI);
     res.json({ message: "Database connected" });
