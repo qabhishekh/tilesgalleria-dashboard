55f1df0fd00f7eda54a48c316c9f1cf0cdb32f68
abhishekh-westack <abhishekh.ojha@westack.ai>
2025-10-07 02:35:14 +0530

    fix: update redirect path after successful login from /dashboard to /invoice

--- tilesgalleria-dashboard/src/pages/Login/Login.jsx
+++ tilesgalleria-dashboard/src/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
       localStorage.setItem("user", JSON.stringify(res.data.user));
 
       toast.success("✅ Login Successful!");
-      window.location = "/dashboard";
+      window.location = "/invoice";
     } catch (err) {
       const msg = err.response?.data?.message || "❌ Login failed";
       setError(msg);
